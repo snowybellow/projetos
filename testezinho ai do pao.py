764ef35670a490f7cb8bbfe0769bcb199b8a7fb4
Snowy <gabrielar2007a@gmail.com>
2026-06-24 16:06:58 -0300

    Add files via upload

---
+++ testezinho ai do pao.py
@@ -0,0 +1,8 @@
+seudinheiro = float(input("quanto ta seu dinheiro? ")) #aqui onde voce colocar um valor imaginario de dinheiro
+valordopao = 2 #aqui é o valor fixo do pão
+
+if valordopao >= 2: #se o valor for maior ou igual a dois
+    print("se compra o pão")
+if valordopao < 2: #se o valor for menor que dois
+    print("voce nao compra o pao")    
+#fim
